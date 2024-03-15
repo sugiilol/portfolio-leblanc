d28c4635bc44856e026c2765f52e097892f53cd9
sugiilol <sugii.noob@gmail.com>
2024-03-15 18:51:09 +0100

    HomePage css updated

--- Portfolio-Leblanc/src/View/HomePage/HomePage.jsx
+++ Portfolio-Leblanc/src/View/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ import linkedin from './imgs/linkedin.png'
 
 export default function HomePage() {
     return (
-        <div className="homepage-container flex flex-row justify-center p-40 gap-20">
+        <div className="homepage-container flex flex-row justify-center px-10 py-40 gap-20">
             <div className='homepage-left-part flex flex-col gap-3 max-w-3xl'>
                 <h1 className='text-6xl font-semibold'>Hi, I'm Sébastien.</h1>
                 <h2 className='text-4xl font-semibold text-cyan-600'>Frontend Developer</h2>
@@ -16,15 +16,18 @@ export default function HomePage() {
                 </p>
                 <div className='homepage-social-link'>
                     <ul className='flex flex-row gap-3 py-5'>
-                        <li className='w-10 h-10' ><a href="#"><img src={linkedin} alt="Linkedin" /></a></li>
-                        <li className='w-10 h-10' ><a href="#"><img src={github} alt="Github" /></a></li>
-                        <li className='w-10 h-10' ><a href="#"><img src={discord} alt="Discord" /></a></li>
-                        <li className='w-10 h-10' ><a href="#"><img src={facebook} alt="Facebook" /></a></li>
+                        <li className='w-10 h-10 rounded-full' ><a href="#"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110'   src={linkedin} alt="Linkedin" /></a></li>
+                        <li className='w-10 h-10 rounded-full' ><a href="#"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110'  src={github} alt="Github" /></a></li>
+                        <li className='w-10 h-10 rounded-full' ><a href="#"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110'  src={discord} alt="Discord" /></a></li>
+                        <li className='w-10 h-10 rounded-full' ><a href="#"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={facebook} alt="Facebook" /></a></li>
                     </ul>
+                </div>
+                <div>
+                    <button className='w-48 h-10 border-2 border-cyan-600 text-cyan-600 rounded-xl transition duration-700 shadow hover:shadow-cyan-600'>Hire me !</button>
                 </div>
             </div>
             <div className='homepage-right-part flex-none rounded-full'>
-                <img className="flex-none rounded-full" src="https://placehold.co/450x450" alt="Leblanc Sébastien" />
+                <img className="flex-none rounded-full transition duration-700 shadow-xl shadow-cyan-600" src="https://placehold.co/450x450" alt="Leblanc Sébastien" />
             </div>
         </div>
     )
