7fc7eff3f86645a7e5da8362043fb3552c24129b
SuGii <sugii.noob@gmail.com>
2024-03-15 07:45:12 +0100

    css updated with Tailwind

--- Portfolio-Leblanc/src/Components/Navbar/Navbar.jsx
+++ Portfolio-Leblanc/src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,19 @@
-import './Navbar.scss'
+// import './Navbar.scss'
 import navbarItems from './navbarItems.json'
 
 
 export default function Navbar() {
 
     return (
-        <div className="navbar-container">
+        <div className="navbar-container p-5 flex flex-row justify-evenly">
             <div className='navbar-left-part'>
-                <a href="#">Leblanc.</a>
+                <a className="text-3xl font-semibold duration-1000 hover:text-cyan-600" href="#">Leblanc.</a>
             </div>
-            <div className='navbar-righ-part'>
-                <ul className='navbar-list-items'>
+            <div className='navbar-righ-part flex items-center'>
+                <ul className='navbar-list-items text-xl flex flex-row gap-5 justify-center justify-items-center'>
                     {navbarItems.map((item, index) => {
                         return (
-                            <li key={index}><a href={item.url}>{item.name}</a></li>
+                            <li className="duration-700 hover:text-cyan-600" key={index}><a href={item.url}>{item.name}</a></li>
                         )
                     })}
                 </ul>
