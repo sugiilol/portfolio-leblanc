bb707080a591094dea7a56538401b5717805d0b9
sugiilol <sugii.noob@gmail.com>
2024-03-19 18:54:59 +0100

    Projects - ProjectCard added

---
+++ Portfolio-Leblanc/src/Components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,11 @@
+export default function ProjectCard(){
+    return(
+        <a href="#">
+            <div className=" flex justify-center items-center w-72 h-96 rounded-2xl">
+                <div className="w-full h-full rounded-2xl bg-[url('/imgs/bateau.png')] bg-contain bg-no-repeat duration-700 hover:scale-105 hover:overflow-hidden">
+
+                </div>
+            </div>
+        </a>      
+    )
+}
---
+++ Portfolio-Leblanc/src/View/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import ProjectCard from "../../Components/ProjectCard/ProjectCard"
+
+export default function Projects(){
+    return(
+        <div className="flex flex-col justify-center items-center px-5">
+            <h2 className="text-5xl font-semibold my-16">Project<span className="text-cyan-600">s</span></h2>
+            <div className="flex flex-row flex-wrap justify-center items-center gap-10">
+                <ProjectCard />
+                <ProjectCard />
+            </div>
+            
+        </div>
+    )
+}
