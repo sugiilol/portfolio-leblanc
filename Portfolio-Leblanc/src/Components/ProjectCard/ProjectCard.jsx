5f6a3a8549c8535b7fdd82a0beb973a182489d38
sugiilol <sugii.noob@gmail.com>
2024-03-20 18:50:04 +0100

    ProjectCard updated

--- Portfolio-Leblanc/src/Components/ProjectCard/ProjectCard.jsx
+++ Portfolio-Leblanc/src/Components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,11 @@
-export default function ProjectCard(){
+export default function ProjectCard(props){
     return(
-        <a href="#">
-            <div className=" flex justify-center items-center w-72 h-96 rounded-2xl">
-                <div className="w-full h-full rounded-2xl bg-[url('/imgs/bateau.png')] bg-contain bg-no-repeat duration-700 hover:scale-105 hover:overflow-hidden">
+        <a href="https://fullmoon-shop.com/fr/" target="_blank">
+            <div className=" flex justify-center items-center w-72 h-96 rounded-2xl ">
+                <div className={`w-full h-full rounded-2xl bg-[url('${props.imageUrl}')] bg-contain bg-no-repeat duration-700 hover:scale-105 hover:overflow-hidden`}>
+                    <div className="w-full h-full rounded-2xl duration-700 hover:bg-black/50">
 
+                    </div>
                 </div>
             </div>
         </a>      
