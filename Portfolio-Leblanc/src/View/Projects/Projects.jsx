4eb982302f9e8d0e53e7d0f8fd65764acae06517
sugiilol <sugii.noob@gmail.com>
2024-03-25 19:01:17 +0100

    bug fixed on img ProjectCard

--- Portfolio-Leblanc/src/View/Projects/Projects.jsx
+++ Portfolio-Leblanc/src/View/Projects/Projects.jsx
@@ -5,8 +5,8 @@ export default function Projects(){
         <div className="flex flex-col justify-center items-center px-5">
             <h2 className="text-5xl font-semibold my-16">Project<span className="text-cyan-600">s</span></h2>
             <div className="flex flex-row flex-wrap justify-center items-center gap-10">
-                <ProjectCard imageUrl='./imgs/bateau.jpg'/>
-                <ProjectCard imageUrl='./imgs/vaisseau.png'/>
+                <ProjectCard imageUrl='/imgs/bateau.jpg'/>
+                <ProjectCard imageUrl='/imgs/vaisseau.png'/>
             </div>         
         </div>
     )
