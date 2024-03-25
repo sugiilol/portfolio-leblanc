73935bcb14807afd6f6da06377b50df88a2bd47c
sugiilol <sugii.noob@gmail.com>
2024-03-25 19:01:17 +0100

    bug fixed on img ProjectCard

--- Portfolio-Leblanc/src/Components/ProjectCard/ProjectCard.jsx
+++ Portfolio-Leblanc/src/Components/ProjectCard/ProjectCard.jsx
@@ -4,7 +4,7 @@ export default function ProjectCard(props){
         <div>
            <a href="https://fullmoon-shop.com/fr/" target="_blank">
                 <div className=" flex justify-center items-center w-72 h-96 rounded-2xl ">
-                    <div className={`w-full h-full rounded-2xl bg-[url('${props.imageUrl}')] bg-contain bg-no-repeat duration-700 hover:scale-105 hover:overflow-hidden`}>
+                    <div className={`w-full h-full rounded-2xl bg-contain bg-no-repeat duration-700 hover:scale-105 hover:overflow-hidden`} style={{backgroundImage: 'url(' + props.imageUrl + ')', }}>
                         <div className="w-full h-full rounded-2xl duration-700 hover:bg-black/50">
 
                         </div>
