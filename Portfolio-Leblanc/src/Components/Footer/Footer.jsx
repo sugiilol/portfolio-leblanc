73d5cc1296d03757a030eb6d67a8f410d5ede50b
SuGii <sugii.noob@gmail.com>
2024-04-08 07:57:35 +0200

    mailto link - logo mail - added to footer

--- Portfolio-Leblanc/src/Components/Footer/Footer.jsx
+++ Portfolio-Leblanc/src/Components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import discord from './imgs/discord.png'
 import facebook from './imgs/facebook.png'
 import github from './imgs/github.png'
 import linkedin from './imgs/linkedin.png'
+import mail from './imgs/mail.png'
 
 export default function Footer() {
     return (
@@ -9,8 +10,8 @@ export default function Footer() {
             <ul className='flex flex-row gap-3'>
                 <li className='w-10 h-10 rounded-full' ><a href="https://www.linkedin.com/in/leblanc-sbt/" target="_blank"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={linkedin} alt="Linkedin" /></a></li>
                 <li className='w-10 h-10 rounded-full' ><a href="https://github.com/sugiilol" target="_blank"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={github} alt="Github" /></a></li>
-                {/* <li className='w-10 h-10 rounded-full' ><a href="#"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={discord} alt="Discord" /></a></li> */}
                 <li className='w-10 h-10 rounded-full' ><a href="https://www.facebook.com/sebastien.leblanc.353250" target="_blank"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={facebook} alt="Facebook" /></a></li>
+                <li className='w-10 h-10 rounded-full' ><a href="mailto:leblanc.sbt@gmail.com"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={mail} alt="mail" /></a></li>
             </ul>
             <ul className='flex flex-row gap-3 py-5'>
                 <li className='duration-700 hover:text-cyan-600'><a href="#homepage-part">About</a></li>
