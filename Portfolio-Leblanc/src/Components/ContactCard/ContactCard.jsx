dbaab9d5aaa539137b1b287541d2a40e7a54952e
SuGii <sugii.noob@gmail.com>
2024-04-09 08:52:34 +0200

    contactCard updated

--- Portfolio-Leblanc/src/Components/ContactCard/ContactCard.jsx
+++ Portfolio-Leblanc/src/Components/ContactCard/ContactCard.jsx
@@ -1,15 +1,20 @@
-export default function ContactCard(){
-    return(
-        <div className="flex flex-row justify-center items-center rounded-xl bg-cyan-900 gap-10 p-5 w-24">
-            <div className="">
-                logo
+import linkedin from './linkedin.png'
+import fleche from './imgs/fleche30x30.png'
+
+export default function ContactCard() {
+    return (
+        <a target="_blank" href="http://www.google.com">
+            <div className="flex flex-row justify-center items-center rounded-xl bg-sugii-blue border-2 drop-shadow-m border-cyan-600 gap-10 p-3 duration-700 hover:scale-105 hover:bg-cyan-950">
+                <div className="">
+                    <img className='w-10 h-10' src={linkedin} alt="linkedin" />
+                </div>
+                <div className="">
+                    /leblanc-sbt/
+                </div>
+                <div className="">
+                    <img className='w-6 h-6' src={fleche} alt="fleche" />
+                </div>
             </div>
-            <div className="">
-                text
-            </div>
-            <div className="">
-                logo
-            </div>
-        </div>
+        </a>
     )
 }   
--- Portfolio-Leblanc/src/View/ContactLink/ContactLink.jsx
+++ Portfolio-Leblanc/src/View/ContactLink/ContactLink.jsx
@@ -1,9 +1,15 @@
 import ContactCard from '../../Components/ContactCard/ContactCard'
 
-export default function ContactLink(){
-    return(
-        <div>
-            <ContactCard />
+export default function ContactLink() {
+    return (
+        <div id="contact-part" className='flex flex-col justify-center items-center mb-12 m-10'>
+            <h2 className="text-5xl font-semibold my-16">Contact <span className="text-cyan-600">me</span></h2>
+            <div className='flex flex-row justify-center items-center gap-5'>
+                <ContactCard />
+                <ContactCard />
+                <ContactCard />
+                <ContactCard />
+            </div>
         </div>
     )
 }
