e325f2427e3302c43f60ee7554b7e9491421539a
SuGii <sugii.noob@gmail.com>
2024-04-10 07:23:49 +0200

    ContactCard updated

--- Portfolio-Leblanc/src/Components/ContactCard/ContactCard.jsx
+++ Portfolio-Leblanc/src/Components/ContactCard/ContactCard.jsx
@@ -4,10 +4,10 @@ import fleche from './imgs/fleche30x30.png'
 export default function ContactCard(props) {
     return (
         <a target="_blank" href={props.link}>
-            <div className="flex flex-row justify-center items-center w-80 rounded-xl bg-sugii-blue border-2 drop-shadow-m border-cyan-600 gap-10 p-3 duration-700 hover:scale-105 hover:bg-cyan-950">
-                <img className='w-10 h-10' src={props.icon} alt={props.alt} />                             
-                {props.title}              
-                <img className='w-6 h-6' src={fleche} alt="fleche" />              
+            <div className="flex flex-row justify-between items-center w-64 rounded-xl bg-sugii-blue border-2 border-cyan-900 gap-10 p-3 duration-700 hover:scale-105 hover:border-cyan-600">
+                <img className='w-10 h-10' src={props.icon} alt={props.alt} />
+                {props.title}
+                <img className='w-6 h-6' src={fleche} alt="fleche" />
             </div>
         </a>
     )
