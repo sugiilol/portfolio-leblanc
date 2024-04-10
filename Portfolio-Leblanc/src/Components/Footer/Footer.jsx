52919bfd833e90e1e3436db2fff8c4bcac4837e2
SuGii <sugii.noob@gmail.com>
2024-04-10 07:23:06 +0200

    Footer updated

--- Portfolio-Leblanc/src/Components/Footer/Footer.jsx
+++ Portfolio-Leblanc/src/Components/Footer/Footer.jsx
@@ -7,12 +7,12 @@ import mail from './imgs/mail.png'
 export default function Footer() {
     return (
         <div className="w-100 flex flex-col items-center justify-center pb-8">
-            <ul className='flex flex-row gap-3'>
+            {/* <ul className='flex flex-row gap-3'>
                 <li className='w-10 h-10 rounded-full' ><a href="https://www.linkedin.com/in/leblanc-sbt/" target="_blank"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={linkedin} alt="Linkedin" /></a></li>
                 <li className='w-10 h-10 rounded-full' ><a href="https://github.com/sugiilol" target="_blank"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={github} alt="Github" /></a></li>
                 <li className='w-10 h-10 rounded-full' ><a href="https://www.facebook.com/sebastien.leblanc.353250" target="_blank"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={facebook} alt="Facebook" /></a></li>
                 <li className='w-10 h-10 rounded-full' ><a href="mailto:leblanc.sbt@gmail.com"><img className='rounded-full transition duration-700 shadow hover:shadow-cyan-600 hover:scale-110' src={mail} alt="mail" /></a></li>
-            </ul>
+            </ul> */}
             <ul className='flex flex-row gap-3 py-5'>
                 <li className='duration-700 hover:text-cyan-600'><a href="#homepage-part">About</a></li>
                 <li className='duration-700 hover:text-cyan-600'><a href="#projects-part">Projects</a></li>
