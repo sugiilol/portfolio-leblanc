32dd393485bb3434de4a586c8e6ec2925fd46198
SuGii <sugii.noob@gmail.com>
2024-04-10 08:14:18 +0200

    Cursus updated

--- Portfolio-Leblanc/src/View/Cursus/Cursus.jsx
+++ Portfolio-Leblanc/src/View/Cursus/Cursus.jsx
@@ -4,7 +4,7 @@ export default function Cursus() {
             <h2 className="text-5xl font-semibold my-16">Cursu<span className="text-cyan-600">s</span></h2>
             <div className="flex flex-col-reverse justify-center items-center gap-20 md:flex-row">
                 <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-3xl font-semibold my-16">Mon parcour<span className="text-cyan-600">s</span></h3>
+                    <h3 className="text-3xl font-semibold my-16">My journe<span className="text-cyan-600">y</span></h3>
                     <ul className="flex flex-col gap-5 justify-center items-center">
                         <li className="bg-cyan-600 w-full rounded-2xl p-4">2024 - 2026 Ecole G4 - Numérique et Gestion de projet RNCP 7</li>
                         <li className="bg-cyan-700 w-full rounded-2xl p-4">2014 - 2015 AFPA - Développeur logiciel RNCP 5</li>
@@ -13,7 +13,7 @@ export default function Cursus() {
                     </ul>
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-3xl font-semibold my-16">Mes experience<span className="text-cyan-600">s</span></h3>
+                    <h3 className="text-3xl font-semibold my-16">My caree<span className="text-cyan-600">r</span></h3>
                     <ul className="flex flex-col gap-5 justify-center items-center">
                         <li className="bg-cyan-600 w-full rounded-2xl p-4">2018 - 2024 FullMoon Distribution - Directeur de Magasin - CDI</li>
                         <li className="bg-cyan-700 w-full rounded-2xl p-4">2016 - 2017 Webgard Agence Web - Développeur Frontend - CDI</li>
