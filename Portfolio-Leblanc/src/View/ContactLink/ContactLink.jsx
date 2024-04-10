b634eddc560c8e9aa0a9423420fc607d06d6e1d4
SuGii <sugii.noob@gmail.com>
2024-04-10 07:23:49 +0200

    ContactCard updated

--- Portfolio-Leblanc/src/View/ContactLink/ContactLink.jsx
+++ Portfolio-Leblanc/src/View/ContactLink/ContactLink.jsx
@@ -5,12 +5,12 @@ console.log(dataContact)
 
 export default function ContactLink() {
     return (
-        <div id="contact-part" className='flex flex-col justify-center items-center mb-12 m-10'>
+        <div id="contact-part" className='flex flex-col justify-center items-center m-20'>
             <h2 className="text-5xl font-semibold my-16">Contact <span className="text-cyan-600">me</span></h2>
-            <div className='flex flex-row flex-wrap justify-center items-center gap-5'>
+            <div className='flex flex-row flex-wrap justify-center items-center gap-5 p-3 rounded-xl'>
                 {dataContact.map((contact, index) => {
-                    return(
-                        <ContactCard key={index} title={contact.title} icon={contact.icon} link={contact.title} alt={contact.alt} />
+                    return (
+                        <ContactCard key={index} title={contact.title} icon={contact.icon} link={contact.link} alt={contact.alt} />
                     )
                 })}
             </div>
