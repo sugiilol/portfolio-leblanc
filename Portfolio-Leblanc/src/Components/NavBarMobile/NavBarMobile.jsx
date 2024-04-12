ffc0ceff0d133decc617b89ff51303c88e99839d
sugiilol <sugii.noob@gmail.com>
2024-04-12 16:47:08 +0200

    navbarMobile updated with bg-color

--- Portfolio-Leblanc/src/Components/NavBarMobile/NavBarMobile.jsx
+++ Portfolio-Leblanc/src/Components/NavBarMobile/NavBarMobile.jsx
@@ -8,12 +8,12 @@ export default function NavBarMobile() {
     let classNavbarMenu = ["absolute", "rounded-r-lg", "w-44", "h-72", "duration-700", "flex", "flex-col", "justify-center", "items-center", "gap-5", "bg-cyan-700/100", "-ml-56"]
 
     if (navbarIsOpen === true) {
-        classNavbarMenu = ["absolute", "rounded-r-lg", "w-44", "h-72", "duration-700", "flex", "flex-col", "justify-center", "items-center", "gap-5", "bg-cyan-700/100", "-ml-10"]
+        classNavbarMenu = ["absolute", "rounded-r-lg", "w-44", "h-72", "duration-700", "flex", "flex-col", "justify-center", "items-center", "gap-5", "bg-cyan-700/100", "-ml-15"]
     }
 
     return (
-        <div className="mt-10 ml-10 fixed md:hidden" onClick={() => setNavbarIsOpen(!navbarIsOpen)}>
-            <button className="flex flex-col gap-1 mb-5" onClick={() => setNavbarIsOpen(!navbarIsOpen)}>
+        <div className="fixed md:hidden bg-sugii-blue w-full" onClick={() => setNavbarIsOpen(!navbarIsOpen)}>
+            <button className="mt-5 ml-10 flex flex-col gap-1 mb-5" onClick={() => setNavbarIsOpen(!navbarIsOpen)}>
                 <span className="h-1 rounded-full w-8 bg-cyan-600"></span>
                 <span className="h-1 rounded-full w-8 bg-cyan-600"></span>
                 <span className="h-1 rounded-full w-8 bg-cyan-600"></span>
